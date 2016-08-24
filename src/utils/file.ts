a06cdd9d26b94cd229a7fa0df4e4911cbdaf8340
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-08-25 01:07:33 +0300

    Add file helper to read file as buffer

---
+++ src/utils/file.ts
@@ -0,0 +1,8 @@
+export function readFileAsBuffer(file: File): Promise<ArrayBuffer> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result as ArrayBuffer);
+    reader.onerror = e => reject(e.error);
+    reader.readAsArrayBuffer(file);
+  });
+}
