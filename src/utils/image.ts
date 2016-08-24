3a676fc4cd14314eba63331dc958924e00879288
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-08-25 02:01:59 +0300

    Add aspect ration to cephalo detection. * Return as soon as possible * Avoid unnecessary image reading

--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,4 +1,5 @@
 require('jimp');
+const url = require('url!./assets/cephalo.jpg');
 
 interface JimpImage {
   _originalMime: 'image/bmp' | 'image/jpeg' | 'image/png',
@@ -15,9 +16,8 @@ declare const Jimp: {
   diff(img1: JimpImage, img2: JimpImage): { percent: number, image: JimpImage };
 };
 
-let _ref: JimpImage | null = null;
-let url = require('url!./assets/cephalo.jpg');
 
+let _ref: JimpImage | null = null;
 async function readReferenceImage(): Promise<JimpImage> {
   if (!_ref) {
     _ref = await Jimp.read(url);
@@ -25,9 +25,20 @@ async function readReferenceImage(): Promise<JimpImage> {
   return _ref;
 }
 
-export async function doesLookLikeCephalometricRadiograph(img: JimpImage): Promise<boolean> {
-  const ref = await readReferenceImage();
-  const distance = Jimp.distance(img, ref);
-  const { percent: diff } = Jimp.diff(img, ref);
-  return (distance < 0.18 || diff < 0.15);
+const getAspectRatio = (img: JimpImage) => img.bitmap.width / img.bitmap.height;
+const getDistance = (img: JimpImage, ref: JimpImage) => Jimp.distance(img, ref);
+const getPixelDiff = (img: JimpImage, ref: JimpImage) => Jimp.diff(img, ref).percent;
+
+function rejectIfFalse(value: boolean): Promise<boolean> {
+  return !value ? Promise.reject(value) : Promise.resolve(value);
+}
+
+export function doesLookLikeCephalometricRadiograph(img: JimpImage): Promise<boolean> {
+  return Promise.all([
+    () => rejectIfFalse(getAspectRatio(img) < 0.73),
+    async () => rejectIfFalse(getDistance(img, await readReferenceImage()) < 0.30),
+    async () => rejectIfFalse(getPixelDiff(img, await readReferenceImage()) < 0.15),
+  ])
+  .then(() => true)
+  .catch(() => false);
 }
