268039761df03add6902a0efc41824063f78595c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-10-20 03:24:57 +0300

    Add webpack-env

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="../case-wizard.d.ts" />
 /// <reference path="globals/es2015/index.d.ts" />
 /// <reference path="globals/expect/index.d.ts" />
-/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/webpack-env/index.d.ts" />
