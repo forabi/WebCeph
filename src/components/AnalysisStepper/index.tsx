a9278452f5ad373995372feb6891c87f7e2e0604
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-10-31 21:27:23 +0200

    Scroll first pending step into view

--- src/components/AnalysisStepper/index.tsx
+++ src/components/AnalysisStepper/index.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
+import { findDOMNode } from 'react-dom';
+import scrollIntoViewIfNeeded from 'scroll-into-view-if-needed';
+
+import * as cx from 'classnames';
 import { List, ListItem } from 'material-ui/List';
-import IconPlayArrow from 'material-ui/svg-icons/av/play-arrow';
-import IconHourglass from 'material-ui/svg-icons/action/hourglass-empty';
 import IconDone from 'material-ui/svg-icons/action/done';
-import { pure } from 'recompose';
+import IconHourglass from 'material-ui/svg-icons/action/hourglass-empty';
+import IconPlayArrow from 'material-ui/svg-icons/av/play-arrow';
+
+import findIndex from 'lodash/findIndex';
+import map from 'lodash/map';
+
 import Props from './props';
 import { getDescriptionForStep, getTitleForStep } from './strings';
-import * as cx from 'classnames';
-import map from 'lodash/map';
 
 const classes = require('./style.scss');
 
@@ -23,44 +28,60 @@ const icons: { [id: string]: JSX.Element } = {
   pending: ICON_PENDING,
 };
 
-export const AnalysisStepper = pure((props: Props) => {
-  const {
-    steps,
-    getStepState,
-    getStepValue,
-    isStepRemovable,
-    onRemoveLandmarkClick, onEditLandmarkClick,
-    onStepMouseEnter, onStepMouseLeave,
-  } = props;
-  return (
-    <div className={cx(classes.root, props.className)}>
-      <List className={classes.list}>
-      {
-        map(steps, step => {
-          const value = getStepValue(step.symbol);
-          const state = getStepState(step.symbol);
-          const isDone = state === 'done';
-          const isRemovable = isDone && isStepRemovable(step.symbol);
-          return (
-            <div key={step.symbol}>
-              <ListItem
-                primaryText={getTitleForStep(step)}
-                secondaryText={getDescriptionForStep(step) || undefined}
-                leftIcon={icons[state]}
-                rightIcon={
-                  (typeof value === 'number' ? 
-                    <span>{value.toFixed(1)}</span> : undefined)
-                }
-                onMouseEnter={isDone ? () => onStepMouseEnter(step.symbol) : undefined}
-                onMouseLeave={isDone ? () => onStepMouseLeave(step.symbol) : undefined}
-              />
-            </div>
-          );
-        })
-      }
-      </List>
-    </div>
-  );
-});
+export class AnalysisStepper extends React.PureComponent<Props, { }> {
+  private firstPending: React.ReactInstance | null;
+
+  public componentDidUpdate() {
+    if (this.firstPending !== null) {
+      const node = findDOMNode(this.firstPending);
+      scrollIntoViewIfNeeded(node, false);
+    }
+  }
+
+  public render() {
+    const {
+      className,
+      steps,
+      getStepState,
+      getStepValue,
+      isStepRemovable,
+      onRemoveLandmarkClick, onEditLandmarkClick,
+      onStepMouseEnter, onStepMouseLeave,
+    } = this.props;
+    const firstPendingIndex = findIndex(steps, (step) => getStepState(step.symbol) === 'current');
+    return (
+      <div className={cx(classes.root, className)}>
+        <List className={classes.list}>
+        {
+          map(steps, (step, i) => {
+            const value = getStepValue(step.symbol);
+            const state = getStepState(step.symbol);
+            const isDone = state === 'done';
+            const isRemovable = isDone && isStepRemovable(step.symbol);
+            return (
+              <div key={step.symbol}>
+                <ListItem
+                  ref={(i === firstPendingIndex || firstPendingIndex === -1) ? this.setFirstPending : undefined}
+                  primaryText={getTitleForStep(step)}
+                  secondaryText={getDescriptionForStep(step) || undefined}
+                  leftIcon={icons[state]}
+                  rightIcon={
+                    (typeof value === 'number' ?
+                      <span>{value.toFixed(1)}</span> : undefined)
+                  }
+                  onMouseEnter={isDone ? () => onStepMouseEnter(step.symbol) : undefined}
+                  onMouseLeave={isDone ? () => onStepMouseLeave(step.symbol) : undefined}
+                />
+              </div>
+            );
+          })
+        }
+        </List>
+      </div>
+    );
+  }
+
+  private setFirstPending = (node: React.ReactInstance | null) => this.firstPending = node;
+};
 
 export default AnalysisStepper;
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="../case-wizard.d.ts" />
+/// <reference path="modules/scroll-into-view-if-needed/index.d.ts" />
 /// <reference path="globals/es2015/index.d.ts" />
 /// <reference path="globals/expect/index.d.ts" />
 /// <reference path="globals/webpack-env/index.d.ts" />
