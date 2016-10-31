51c7a7d2bbd6e037eea1f810c720871df187cfbe
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-10-31 19:44:40 +0200

    Scroll to 50% 50% on image load

--- src/components/CephaloCanvasContainer/index.tsx
+++ src/components/CephaloCanvasContainer/index.tsx
@@ -4,19 +4,39 @@ import CephaloCanvas from 'components/CephaloCanvas/connected';
 import CephaloDropzone from 'components/CephaloDropzone/connected';
 import * as cx from 'classnames';
 import Props from './props';
-import { pure } from 'recompose';
+import { findDOMNode } from 'react-dom';
 
 const classes = require('./style.scss');
 
-const CephaloCanvasContainer = pure(({ className, hasImage, onResize }: Props) => {
-  return (
-    <ResizeObservable
-      className={cx(className, classes.root)}
-      onResize={({ contentRect: { width, height } }) => onResize(width, height)}
-    >
-      {hasImage ? <CephaloCanvas /> : <CephaloDropzone />}
-    </ResizeObservable>
-  );
-});
+class CephaloCanvasContainer extends React.PureComponent<Props, { }> {
+  private childInstance: React.ReactInstance | null;
+
+  public componentDidUpdate() {
+    if (this.childInstance === null) {
+      return;
+    }
+    const node = findDOMNode(this.childInstance);
+    const { scrollWidth, scrollHeight, clientWidth, clientHeight }  = node;
+    node.scrollTo((scrollWidth - clientWidth) / 2, (scrollHeight - clientHeight) / 2);
+  }
+
+  public render() {
+    const { hasImage, className } = this.props;
+    return (
+      <ResizeObservable
+        ref={hasImage ? this.setRef : undefined}
+        className={cx(className, classes.root)}
+        onResize={this.handleResize}
+      >
+        {hasImage ? <CephaloCanvas /> : <CephaloDropzone />}
+      </ResizeObservable>
+    );
+  }
+
+  private setRef = (instance: React.ReactInstance | null) => this.childInstance = instance;
+
+  private handleResize: (e: ResizeObserverEntry) => any
+    = ({ contentRect: { width, height } }) => this.props.onResize(width, height);
+};
 
 export default CephaloCanvasContainer;
