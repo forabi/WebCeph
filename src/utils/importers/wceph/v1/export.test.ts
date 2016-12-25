76228aea6f0828d6705791a927dcd78d15d7f7e5
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2016-12-25 17:00:43 +0200

    Use describe in tests

--- src/utils/importers/wceph/v1/export.test.ts
+++ src/utils/importers/wceph/v1/export.test.ts
@@ -8,58 +8,64 @@ import { readFileAsDataURL } from 'utils/file';
 
 import find from 'lodash/find';
 
-it('WCeph Exporter', async () => {
-  const url = require('file-loader!./fixrures/images/ceph1.jpg');
-  const imageFile = new File([await (await fetch(url)).blob()], 'Export test.jpg');
-  const state: Partial<StoreState> = {
-    'workspace.mode': 'tracing',
-    'workspace.images.activeImageId': 'img_1',
-    'workspace.images.props': {
-      img_1: {
-        name: 'Patient 1',
-        type: 'ceph_lateral',
-        flipX: true,
-        flipY: false,
-        brightness: 0.7,
-        contrast: 0.1,
-        height: 500,
-        width: 700,
-        scaleFactor: null,
-        invertColors: true,
-        analysis: {
-          activeId: 'downs',
+describe('WCeph Exporter', () => {
+  it('should be able to export a valid WCeph v1 file', async () => {
+    const url = require('file-loader!./fixrures/images/ceph1.jpg');
+    const imageFile = new File([await (await fetch(url)).blob()], 'Export test.jpg');
+    const state: Partial<StoreState> = {
+      'workspace.mode': 'tracing',
+      'workspace.images.activeImageId': 'img_1',
+      'workspace.images.props': {
+        img_1: {
+          name: 'Patient 1',
+          type: 'ceph_lateral',
+          flipX: true,
+          flipY: false,
+          brightness: 0.7,
+          contrast: 0.1,
+          height: 500,
+          width: 700,
+          scaleFactor: null,
+          invertColors: true,
+          analysis: {
+            activeId: 'downs',
+          },
+          data: await readFileAsDataURL(imageFile),
         },
-        data: await readFileAsDataURL(imageFile),
       },
-    },
-    'workspace.images.tracing': {
-      img_1: {
-        mode: 'assisted',
-        manualLandmarks: {
-          N: {
-            x: 400,
-            y: 300,
+      'workspace.images.tracing': {
+        img_1: {
+          mode: 'assisted',
+          manualLandmarks: {
+            N: {
+              x: 400,
+              y: 300,
+            },
+          },
+          skippedSteps: {
+            S: true,
           },
         },
-        skippedSteps: {
-          S: true,
-        },
       },
-    },
-  };
-  const exportedFile = await exportFile(state as StoreState, { });
-  expect(exportedFile).toBeA(File);
-  expect(exportedFile.name).toBe('Patient 1.wceph');
-  const actions = await importFile(exportedFile, { });
-  expect(actions).toExist();
-  expect(find(actions, action => isActionOfType(action, 'SET_IMAGE_PROPS'))).toBeTruthy();
-  for (const action of actions) {
-    if (isActionOfType(action, 'SET_IMAGE_PROPS')) {
-      expect(action.payload.type).toBe('ceph_lateral');
-      // tslint:disable no-string-literal
-      expect(action.payload.tracing!.manualLandmarks!['N']).toMatch({ x: 400, y: 300 });
-      expect(action.payload.tracing!.skippedSteps!['S']).toBe(true);
-      expect(action.payload.flipX).toBe(true);
-    }
-  }
+    };
+    const exportedFile = await exportFile(state as StoreState, { });
+    expect(exportedFile).toBeA(File);
+    expect(exportedFile.name).toBe('Patient 1.wceph');
+
+    it('should be able to import the exported file correctly', async () => {
+      const actions = await importFile(exportedFile, { });
+      expect(actions).toExist();
+      expect(find(actions, action => isActionOfType(action, 'SET_IMAGE_PROPS'))).toBeTruthy();
+      for (const action of actions) {
+        if (isActionOfType(action, 'SET_IMAGE_PROPS')) {
+          expect(action.payload.type).toBe('ceph_lateral');
+          // tslint:disable no-string-literal
+          expect(action.payload.tracing!.manualLandmarks!['N']).toMatch({ x: 400, y: 300 });
+          expect(action.payload.tracing!.skippedSteps!['S']).toBe(true);
+          expect(action.payload.flipX).toBe(true);
+        }
+      }
+    });
+  });
 });
+
--- src/utils/importers/wceph/v1/import.test.ts
+++ src/utils/importers/wceph/v1/import.test.ts
@@ -5,21 +5,22 @@ import { isActionOfType } from 'utils/store';
 
 const url = require('file-loader!./fixtures/valid/1.wceph');
 
-it('WCeph Importer', async () => {
-  const blob = await (await fetch(url)).blob();
-  const file = new File([blob], 'Test WCeph 1.wceph');
-  const actions = await importFile(file, { });
-  expect(actions.length).toBeGreaterThan(0);
-  for (const action of actions) {
-    if (isActionOfType(action, 'SET_IMAGE_PROPS')) {
-      expect(action.payload.tracing).toExist();
-      expect(action.payload.tracing!.manualLandmarks).toExist();
-      // tslint:disable-next-line no-string-literal
-      const N = action.payload.tracing!.manualLandmarks!['N'];
-      expect(N).toExist();
-      expect(isV1GeometricalPoint(N)).toBe(true);
-      expect(action.payload.flipX).toBe(true);
-      // expect(action.payload.tracing).toMatchSnapshot();
+describe('WCeph Importer', () => {
+  it('should be able to import a valid WCeph v1 file', async () => {
+    const blob = await (await fetch(url)).blob();
+    const file = new File([blob], 'Test WCeph 1.wceph');
+    const actions = await importFile(file, { });
+    expect(actions.length).toBeGreaterThan(0);
+    for (const action of actions) {
+      if (isActionOfType(action, 'SET_IMAGE_PROPS')) {
+        expect(action.payload.tracing).toExist();
+        expect(action.payload.tracing!.manualLandmarks).toExist();
+        // tslint:disable-next-line no-string-literal
+        const N = action.payload.tracing!.manualLandmarks!['N'];
+        expect(N).toExist();
+        expect(isV1GeometricalPoint(N)).toBe(true);
+        expect(action.payload.flipX).toBe(true);
+      }
     }
-  }
+  });
 });
