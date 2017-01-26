d1291b9a1d0b941ce4750d23e2c861b755dd849c
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-01-26 21:00:42 +0200

    Activate first workspace automatically

--- src/store/reducers/workspace/activeId.ts
+++ src/store/reducers/workspace/activeId.ts
@@ -5,6 +5,7 @@ const KEY_ACTIVE_WORKSPACE_ID: StoreKey = 'workspaces.activeWorkspaceId';
 const reducers: Partial<ReducerMap> = {
   [KEY_ACTIVE_WORKSPACE_ID]: handleActions<typeof KEY_ACTIVE_WORKSPACE_ID>({
     SET_ACTIVE_WORKSPACE: (_, { payload: { id } }) => id,
+    ADD_NEW_WORKSPACE: (state, { payload: { id } }) => state === null ? id : state,
   }, null),
 };
 
