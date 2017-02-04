a7ed9aacdf55575de8615870c3b8b23962063e29
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-02-04 12:42:21 +0200

    Update styles to match fabric

--- src/components/App/Progress.tsx
+++ src/components/App/Progress.tsx
@@ -1,6 +1,21 @@
 import * as React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-const Progress = () => <CircularProgress size={70} />;
+const scaleInAndRotate = require('transitions/scale-in-and-rotate.scss');
+
+const Progress = () => (
+  <ReactCSSTransitionGroup
+    className={scaleInAndRotate.root}
+    transitionAppear
+    transitionLeave
+    transitionName={scaleInAndRotate}
+    transitionAppearTimeout={1000}
+    transitionEnterTimeout={1000}
+    transitionLeaveTimeout={1000}
+  >
+    <CircularProgress thickness={2} color="#0078d7" size={60} />
+  </ReactCSSTransitionGroup>
+);
 
 export default Progress;
