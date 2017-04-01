0140901bf69c149b0580ece91c2659e48a871d47
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-04-01 21:12:57 +0300

    Switch to empty workspace if new one is requested

--- src/store/middleware/workspaceManager.ts
+++ src/store/middleware/workspaceManager.ts
@@ -4,19 +4,24 @@ import { isLastWorkspaceUsed, getLastWorkspaceId } from 'store/reducers/workspac
 
 import { isActionOfType } from 'utils/store';
 
+import { setActiveWorkspace } from 'actions/workspace';
+
 const middleware: Middleware = ({ getState }: Store<StoreState>) =>
   (next: Dispatch<GenericAction>) => (action: GenericAction) => {
     if (isActionOfType(action, 'ADD_NEW_WORKSPACE')) {
       const state = getState();
       const lastWorkspaceId = getLastWorkspaceId(state);
+      const { payload: { id } } = action;
       if (lastWorkspaceId === null || isLastWorkspaceUsed(state)) {
-        return next(action);
+        next(action);
+        return next(setActiveWorkspace({ id }));
+      } else {
+        console.info(
+          'An empty workspace is available. ' +
+          'Switching to that workspace instead of creating a new one.',
+        );
+        return next(setActiveWorkspace({ id: lastWorkspaceId }));
       }
-      console.info(
-        'An empty workspace is available. ' +
-        'Ignoring request to add a new one.',
-      );
-      return;
     } else {
       return next(action);
     }
