913873023ca69646ad22c5f9dfad8ccaee5b8b67
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-04-03 21:37:10 +0300

    Remove old typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,2 @@
-/// <reference path="../case-wizard.d.ts" />
-/// <reference path="globals/es2015/index.d.ts" />
-/// <reference path="globals/expect/index.d.ts" />
 /// <reference path="globals/webpack-env/index.d.ts" />
 /// <reference path="modules/scroll-into-view-if-needed/index.d.ts" />
