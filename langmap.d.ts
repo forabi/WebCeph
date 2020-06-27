5e1f3f7e226fa09cfbba0b733b0f9ec96facc999
bebeo05 <hdthong@gmail.com>
2020-06-28 01:42:18 +0700

    ff ff

---
+++ langmap.d.ts
@@ -0,0 +1,8 @@
+declare module 'langmap' {
+  type langDef = {
+    nativeName: string;
+    englishName: string;
+  };
+  const langmap: Record<string, langDef | undefined>;
+  export = langmap;
+}
