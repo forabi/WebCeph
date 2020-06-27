3d7d634390f5e9102e1dd9136f04151cc2246685
bebeo05 <hdthong@gmail.com>
2020-06-27 23:57:26 +0700

    test

---
+++ src/utils/store.ts
@@ -0,0 +1,29 @@
+export function createAction<T extends ActionType>(type: T, payload: Events[T]): Action<T> {
+  return {
+    type,
+    payload,
+  };
+};
+
+export function createActionCreator<T extends ActionType>(type: T): ActionCreator<T> {
+  return (payload: Events[T]) => createAction<T>(type, payload);
+};
+
+export function handleActions<Key extends StoreKey>(
+  map: ActionToReducerMap<Key>, defaultState: StoreState[Key],
+): Reducer<StoreState[Key], ActionType> {
+  return (state: StoreState[Key], action: Action<ActionType>) => {
+    const reducer: Reducer<StoreState[Key], typeof action['type']> | undefined = map[action.type];
+    if (typeof reducer !== 'undefined') {
+      return reducer(state, action);
+    }
+    if (typeof state !== 'undefined') {
+      return state;
+    }
+    return defaultState;
+  };
+}
+
+export function isActionOfType<T extends ActionType>(action: GenericAction, type: T): action is Action<T> {
+  return type === action.type;
+}
