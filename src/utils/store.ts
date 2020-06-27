43fe2634e641796f6dbc1978d7c2ec44c6518599
bebeo05 <hdthong@gmail.com>
2020-06-28 01:54:26 +0700

    sds

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -1,29 +1,39 @@
-export function createAction<T extends ActionType>(type: T, payload: Events[T]): Action<T> {
+export function createAction<T extends ActionType>(
+  type: T,
+  payload: Events[T]
+): Action<T> {
   return {
     type,
-    payload,
+    payload
   };
-};
+}
 
-export function createActionCreator<T extends ActionType>(type: T): ActionCreator<T> {
+export function createActionCreator<T extends ActionType>(
+  type: T
+): ActionCreator<T> {
   return (payload: Events[T]) => createAction<T>(type, payload);
-};
+}
 
 export function handleActions<Key extends StoreKey>(
-  map: ActionToReducerMap<Key>, defaultState: StoreState[Key],
+  map: ActionToReducerMap<Key>,
+  defaultState: StoreState[Key]
 ): Reducer<StoreState[Key], ActionType> {
   return (state: StoreState[Key], action: Action<ActionType>) => {
-    const reducer: Reducer<StoreState[Key], typeof action['type']> | undefined = map[action.type];
-    if (typeof reducer !== 'undefined') {
+    const reducer: Reducer<StoreState[Key], typeof action["type"]> | undefined =
+      map[action.type];
+    if (typeof reducer !== "undefined") {
       return reducer(state, action);
     }
-    if (typeof state !== 'undefined') {
+    if (typeof state !== "undefined") {
       return state;
     }
     return defaultState;
   };
 }
 
-export function isActionOfType<T extends ActionType>(action: GenericAction, type: T): action is Action<T> {
+export function isActionOfType<T extends ActionType>(
+  action: GenericAction,
+  type: T
+): action is Action<T> {
   return type === action.type;
 }
